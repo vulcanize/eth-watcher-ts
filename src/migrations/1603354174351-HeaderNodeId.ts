71b89bc2d0f0ea45406a6176e07bec38b3555f4d
ramil <ramilexe@gmail.com>
2020-10-23 18:59:26 +0300

    fix migration for postgraphile

--- src/migrations/1603354174351-HeaderNodeId.ts
+++ src/migrations/1603354174351-HeaderNodeId.ts
@@ -4,8 +4,8 @@ export class HeaderNodeId1603354174351 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         // Fix for postgraphile
-        await queryRunner.query(`COMMENT ON TABLE "data"."header" IS E'Data Header';`);
-        await queryRunner.query(`COMMENT ON COLUMN "data"."header"."node_id" IS E'Header Node Id';`);
+        await queryRunner.query(`COMMENT ON TABLE data.header IS E'@name Data Header';`);
+        await queryRunner.query(`COMMENT ON COLUMN data.header.node_id IS E'@name ChainNodeID';`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
