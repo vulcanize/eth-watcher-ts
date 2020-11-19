f88d6e262bb2cee8be081ad30296165f882a0799
Ilnur Galiev <ilnur.a.g@gmail.com>
2020-11-19 04:51:39 +0300

    add functions toStructure and toFields

--- src/services/dataTypeParser.test.ts
+++ src/services/dataTypeParser.test.ts
@@ -1,43 +1,101 @@
-import { parse, errUnknownVariable, errUnknownStruct } from './dataTypeParser';
+import { toStructure, toFields, } from './dataTypeParser';
 
-describe('dataTypeParser.parse', function () {
-
-  test('empty', function () {
-    expect(parse('', 'someName')).toStrictEqual([]);
-    expect(parse(null, 'someName')).toStrictEqual([]);
-    expect(parse('      ', 'someName')).toStrictEqual([]);
-  });
-
-  test('bad data types', function () {
-    expect(parse.bind(null, 'asd', 'someVar')).toThrow(/mismatched/);
-    expect(parse.bind(null, 'uint public', 'someVar')).toThrow(/extraneous input/);
-    expect(parse.bind(null, 'uint public a', 'someVar')).toThrow(/mismatched/);
-  });
-
-  test('not found', function () {
-    expect(parse.bind(null, 'string public constant name;', 'someVar')).toThrow(errUnknownVariable);
-    expect(parse.bind(null, 'Checkpoint chck;', 'chck')).toThrow(errUnknownStruct);
-  });
-
+describe('dataTypeParser', function () {
   test('elementary types', function () {
-    expect(parse('string public constant name = "Uniswap";', 'name'))
-      .toStrictEqual([{ name: 'name', type: 'string' }]);
-    expect(parse('uint8 public constant decimals = 18;', 'decimals'))
-      .toStrictEqual([{ name: 'decimals', type: 'uint8' }]);
+    const st1 = toStructure('string public constant name = "Uniswap";', 'name');
+    expect(st1).toStrictEqual({
+      "kind": "string",
+      "name": "name",
+      "type": "simple"
+    });
+    expect(toFields(st1)).toStrictEqual([{ "name": "name", "type": "string" }]);
+
+    const st2 = toStructure('uint8 public constant decimals = 18;', 'decimals');
+    expect(st2).toStrictEqual({
+      "kind": "uint8",
+      "name": "decimals",
+      "type": "simple"
+    });
+    expect(toFields(st2)).toStrictEqual([{ "name": "decimals", "type": "uint8" }]);
   });
 
   test('array types', function () {
-    expect(parse('string[] public names = ["Uniswap"];', 'names'))
-      .toStrictEqual([{ name: 'key0', type: 'uint' }, { name: 'value0', type: 'string' }]);
-    expect(parse('string[][] public names;', 'names'))
-      .toStrictEqual([{ name: 'key0', type: 'uint' }, { name: 'key1', type: 'uint' }, { name: 'value1', type: 'string' }]);
+    const st1 = toStructure('string[] public names = ["Uniswap"];', 'names');
+    expect(st1).toStrictEqual({
+      "kind": {
+        "kind": "string",
+        "name": "value0",
+        "type": "simple"
+      },
+      "name": "names",
+      "type": "array"
+    });
+    expect(toFields(st1)).toStrictEqual([
+      { "name": "key0", "type": "uint" },
+      { "name": "value0", "type": "string" },
+    ]);
+
+    const st2 = toStructure('string[][] public names;', 'names');
+    expect(st2).toStrictEqual({
+      "kind": {
+        "kind": {
+          "kind": "string",
+          "name": "value1",
+          "type": "simple"
+        },
+        "name":
+          "value0",
+        "type": "array"
+      },
+      "name": "names",
+      "type": "array"
+    });
+    expect(toFields(st2)).toStrictEqual([
+      { "name": "key0", "type": "uint" },
+      { "name": "key1", "type": "uint" },
+      { "name": "value1", "type": "string" },
+    ]);
   });
 
   test('mapping types', function () {
-    expect(parse('mapping (address => uint96) internal balances;', 'balances'))
-      .toStrictEqual([{ name: 'key0', type: 'address' }, { name: 'value0', type: 'uint96' }]);
-    expect(parse('mapping (address => mapping (address => uint96)) internal allowances;', 'allowances'))
-      .toStrictEqual([{ name: 'key0', type: 'address' }, { name: 'key1', type: 'address' }, { name: 'value1', type: 'uint96' }]);
+    const st1 = toStructure('mapping (address => uint96) internal balances;', 'balances');
+    expect(st1).toStrictEqual({
+      "key": "address",
+      "name": "balances",
+      "type": "mapping",
+      "value": {
+        "kind": "uint96",
+        "name": "value0",
+        "type": "simple"
+      }
+    });
+    expect(toFields(st1)).toStrictEqual([
+      { "name": "key0", "type": "address" },
+      { "name": "value0", "type": "uint96" },
+    ]);
+
+    const st2 = toStructure('mapping (address => mapping (address => uint96)) internal allowances;', 'allowances');
+    expect(st2).toStrictEqual({
+      "key": "address",
+      "name": "allowances",
+      "type": "mapping",
+      "value": {
+        "key": "address",
+        "name": "value0",
+        "type": "mapping",
+        "value": {
+          "kind":
+            "uint96",
+          "name": "value1",
+          "type": "simple"
+        }
+      }
+    });
+    expect(toFields(st2)).toStrictEqual([
+      { "name": "key0", "type": "address" },
+      { "name": "key1", "type": "address" },
+      { "name": "value1", "type": "uint96" },
+    ]);
   });
 
   test('user defined types', function () {
@@ -48,9 +106,18 @@ describe('dataTypeParser.parse', function () {
         uint96 votes;
       }
     `;
-    expect(parse(vars1, 'checkpoint')).toStrictEqual([
-      { name: 'fromBlock', type: 'uint32' },
-      { name: 'votes', type: 'uint96' }
+    const st1 = toStructure(vars1, 'checkpoint');
+    expect(st1).toStrictEqual({
+      "fields": [
+        { "kind": "uint32", "name": "fromBlock", "type": "simple" },
+        { "kind": "uint96", "name": "votes", "type": "simple" }
+      ],
+      "name": "checkpoint",
+      "type": "struct"
+    });
+    expect(toFields(st1)).toStrictEqual([
+      { "name": "votes", "type": "uint96" },
+      { "name": "fromBlock", "type": "uint32" }
     ]);
 
     const vars2 = `
@@ -60,10 +127,23 @@ describe('dataTypeParser.parse', function () {
         uint96 votes;
       }
     `;
-    expect(parse(vars2, 'checkpoint')).toStrictEqual([
-      { name: 'key0', type: 'uint' },
-      { name: 'fromBlock', type: 'uint32' },
-      { name: 'votes', type: 'uint96' }
+    const st2 = toStructure(vars2, 'checkpoint');
+    expect(st2).toStrictEqual({
+      "kind": {
+        "fields": [
+          { "kind": "uint32", "name": "fromBlock", "type": "simple" },
+          { "kind": "uint96", "name": "votes", "type": "simple" }
+        ],
+        "name": "value0",
+        "type": "struct"
+      },
+      "name": "checkpoint",
+      "type": "array"
+    });
+    expect(toFields(st2)).toStrictEqual([
+      { "name": "key0", "type": "uint" },
+      { "name": "votes", "type": "uint96" },
+      { "name": "fromBlock", "type": "uint32" }
     ]);
 
     const vars3 = `
@@ -73,10 +153,24 @@ describe('dataTypeParser.parse', function () {
         uint96 votes;
       }
     `;
-    expect(parse(vars3, 'checkpoint')).toStrictEqual([
-      { name: 'key0', type: 'address' },
-      { name: 'fromBlock', type: 'uint32' },
-      { name: 'votes', type: 'uint96' }
+    const st3 = toStructure(vars3, 'checkpoint');
+    expect(st3).toStrictEqual({
+      "key": "address",
+      "name": "checkpoint",
+      "type": "mapping",
+      "value": {
+        "fields": [
+          { "kind": "uint32", "name": "fromBlock", "type": "simple" },
+          { "kind": "uint96", "name": "votes", "type": "simple" }
+        ],
+        "name": "value0",
+        "type": "struct"
+      }
+    });
+    expect(toFields(st3)).toStrictEqual([
+      { "name": "key0", "type": "address" },
+      { "name": "votes", "type": "uint96" },
+      { "name": "fromBlock", "type": "uint32" }
     ]);
 
     const vars4 = `
@@ -86,11 +180,30 @@ describe('dataTypeParser.parse', function () {
         uint96 votes;
       }
     `;
-    expect(parse(vars4, 'checkpoint')).toStrictEqual([
-      { name: 'key0', type: 'address' },
-      { name: 'key1', type: 'uint' },
-      { name: 'fromBlock', type: 'uint32' },
-      { name: 'votes', type: 'uint96' }
+    const st4 = toStructure(vars4, 'checkpoint');
+    expect(st4).toStrictEqual({
+      "key": "address",
+      "name": "checkpoint",
+      "type": "mapping",
+      "value": {
+        "key": "uint",
+        "name": "value0",
+        "type": "mapping",
+        "value": {
+          "fields": [
+            { "kind": "uint32", "name": "fromBlock", "type": "simple" },
+            { "kind": "uint96", "name": "votes", "type": "simple" }
+          ],
+          "name": "value1",
+          "type": "struct"
+        }
+      }
+    });
+    expect(toFields(st4)).toStrictEqual([
+      { "name": "key0", "type": "address" },
+      { "name": "key1", "type": "uint" },
+      { "name": "votes", "type": "uint96" },
+      { "name": "fromBlock", "type": "uint32" }
     ]);
 
     const vars5 = `
@@ -103,12 +216,30 @@ describe('dataTypeParser.parse', function () {
         uint96 votes;
       }
     `;
-    expect(parse(vars5, 'checkpoint')).toStrictEqual([
-      { name: 'key0', type: 'address' },
-      { name: 'key1', type: 'uint' },
-      { name: 'fromBlock', type: 'uint32' },
-      { name: 'votes', type: 'uint96' }
+    const st5 = toStructure(vars5, 'checkpoint');
+    expect(st5).toStrictEqual({
+      "key": "address",
+      "name": "checkpoint",
+      "type": "mapping",
+      "value": {
+        "key": "uint",
+        "name": "value0",
+        "type": "mapping",
+        "value": {
+          "fields": [
+            { "kind": "uint32", "name": "fromBlock", "type": "simple" },
+            { "fields": [{ "kind": "uint96", "name": "votes", "type": "simple" }], "name": "votes", "type": "struct" }
+          ],
+          "name": "value1",
+          "type": "struct"
+        }
+      }
+    });
+    expect(toFields(st5)).toStrictEqual([
+      { "name": "key0", "type": "address" },
+      { "name": "key1", "type": "uint" },
+      { "name": "votes", "type": "uint96" },
+      { "name": "fromBlock", "type": "uint32" }
     ]);
   });
 })
-
