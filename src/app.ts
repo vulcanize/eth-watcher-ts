737b8566d555ae1341cc05b3c70e83c99acebc62
Mikhail Reenko <reenko@yandex.ru>
2020-12-09 20:13:59 +0700

    Clean up a code

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as express from 'express';
-import * as path from 'path';
 
 import { INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-status-codes';
 import Routes from './routes';
@@ -43,6 +42,5 @@ export default class App {
 		this.app.use(cors());
 		this.app.use(bodyParser.json());
 		this.app.use(bodyParser.urlencoded({ extended: false }));
-		this.app.use(express.static('public'));
 	}
 }
