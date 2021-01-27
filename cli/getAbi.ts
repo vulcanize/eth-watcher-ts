0475447557acf6acbad86edd77cd7da6757a6f9c
Mikhail Reenko <reenko@yandex.ru>
2021-01-27 19:21:21 +0700

    VUL-196 Get Starting Block and Source Code

--- cli/getAbi.ts
+++ cli/getAbi.ts
@@ -17,15 +17,38 @@ if (!address) {
 }
 
 (async (): Promise<void> => {
-		const uri = `https://api.etherscan.io/api?module=contract&action=getabi&address=${address}&apikey=${apiKey}`;
-		const response = await fetch(uri, { method: 'get' });
+    const uri = `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${address}&apikey=${apiKey}`
+    const response = await fetch(uri, { method: 'get' });
 
-        const data = await response.json();
+    const data = await response.json();
 
-		if (!data.result) {
-            throw new Error('Oops');
-        }
+    if (!data.result || data.result.length === 0) {
+        throw new Error('Oops');
+    }
 
-        const contractABI = JSON.parse(data.result);
-        console.log(contractABI);
+    const contract = data.result[0];
+
+    const contractName = contract.ContractName;
+    const contractABI = JSON.parse(contract.ABI);
+    const sourceCode = contract.SourceCode;
+
+    console.log(contractName);
+    console.log(contractABI);
+    console.log(sourceCode);
+})();
+
+(async (): Promise<void> => {
+    const uri = `https://api.etherscan.io/api?module=account&action=txlist&page=1&offset=3&sort=asc&address=${address}&apikey=${apiKey}`
+    const response = await fetch(uri, { method: 'get' });
+
+    const data = await response.json();
+
+    if (!data.result || data.result.length === 0) {
+        throw new Error('Oops');
+    }
+
+    const firstTx = data.result[0];
+    const blockNumber = firstTx.blockNumber;
+
+    console.log('First block number', blockNumber);
 })();
