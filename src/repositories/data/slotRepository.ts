245298eb27405160737391c76dc426dd6016a692
ramil <ramilexe@gmail.com>
2021-03-05 15:40:44 +0300

    fix linter error

--- src/repositories/data/slotRepository.ts
+++ src/repositories/data/slotRepository.ts
@@ -8,9 +8,8 @@ export default class SlotRepository {
 		this.queryRunner = queryRunner;
     }
 
-	public async add(tableName: string, name: string[], value: any[]): Promise<number> {
+	public async add(tableName: string, name: string[], value: {name; value}[]): Promise<number> {
 		const sql = `INSERT INTO ${tableName} (${name.join(',')}) VALUES ('${value.map((v) => v.toString().replace(/\0/g, '')).join('\',\'')}') RETURNING id;`;
-		console.log(sql);
 
 		const res = await this.queryRunner.query(sql);
 		if (!res) {
