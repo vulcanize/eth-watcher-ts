0aa168a51c13474711f8734505317ddc6a62b77c
ramil <ramilexe@gmail.com>
2021-03-05 15:48:41 +0300

    fix casting error

--- src/repositories/data/slotRepository.ts
+++ src/repositories/data/slotRepository.ts
@@ -8,7 +8,7 @@ export default class SlotRepository {
 		this.queryRunner = queryRunner;
     }
 
-	public async add(tableName: string, name: string[], value: {name; value}[]): Promise<number> {
+	public async add(tableName: string, name: string[], value: string[]): Promise<number> {
 		const sql = `INSERT INTO ${tableName} (${name.join(',')}) VALUES ('${value.map((v) => v.toString().replace(/\0/g, '')).join('\',\'')}') RETURNING id;`;
 
 		const res = await this.queryRunner.query(sql);
