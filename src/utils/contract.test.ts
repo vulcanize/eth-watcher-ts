5cdf036a30101a87d518dbd6ac2f0b0af4b0ba23
nikugogoi <95nikass@gmail.com>
2021-04-27 20:23:42 +0530

    Added more state declarations in test.

--- src/utils/contract.test.ts
+++ src/utils/contract.test.ts
@@ -7,6 +7,16 @@ describe('getStatesfromSourceCode', () => {
       string public constant name = "Uniswap";
 
       uint8 public constant decimals = 18;
+
+      int256 public immutable integers = -21;
+
+      bool internal constant boolean = true;
+
+      address private constant plainAddress = '0x1ca7c995f8eF0A2989BbcE08D5B7Efe50A584aa1'
+
+      address payable constant payableAddress = '0xbb38B6F181541a6cEdfDac0b94175B2431Aa1A02'
+
+      bytes32 constant byteText = "ByteText";
     }
     `;
 
@@ -14,6 +24,15 @@ describe('getStatesfromSourceCode', () => {
     expect(states).toStrictEqual([
       { slot: 0, type: 'string name;', variable: 'name' },
       { slot: 1, type: 'uint8 decimals;', variable: 'decimals' },
+      { slot: 2, type: 'int256 integers;', variable: 'integers' },
+      { slot: 3, type: 'bool boolean;', variable: 'boolean' },
+      { slot: 4, type: 'address plainAddress;', variable: 'plainAddress' },
+      {
+        slot: 5,
+        type: 'address payableAddress;',
+        variable: 'payableAddress'
+      },
+      { slot: 6, type: 'bytes32 byteText;', variable: 'byteText' }
     ])
   })
 
@@ -23,13 +42,16 @@ describe('getStatesfromSourceCode', () => {
       string[] public names = ["Uniswap"];
 
       string[][] public nestedNames;
+
+      uint[] public decimalList;
     }
     `;
 
     const states = await getStatesFromSourceCode(sourceCode);
     expect(states).toStrictEqual([
       { slot: 0, type: 'string[] names;', variable: 'names' },
-      { slot: 1, type: 'string[] nestedNames;', variable: 'nestedNames' }
+      { slot: 1, type: 'string[] nestedNames;', variable: 'nestedNames' },
+      { slot: 2, type: 'uint[] decimalList;', variable: 'decimalList' }
     ])
   })
 
@@ -60,6 +82,10 @@ describe('getStatesfromSourceCode', () => {
   test('user defined types', async () => {
     const sourceCode = `
     contract Test {
+      enum ActionChoices { GoLeft, GoRight, GoStraight, SitStill }
+
+      ActionChoices choice;
+
       struct Checkpoint {
         uint32 fromBlock;
         uint96 votes;
@@ -83,6 +109,19 @@ describe('getStatesfromSourceCode', () => {
       }
 
       mapping(address => mapping(uint => NestedCheckpoint)) public nestedCheckpointNestedMap;
+
+      struct KeyFlag {
+        uint key;
+        bool deleted;
+      }
+
+      struct ComplexCheckpoint {
+        uint32 fromBlock;
+        mapping(uint => SomeVotes) votes;
+        KeyFlag[] keys;
+      }
+
+      mapping(address => ComplexCheckpoint) public complexCheckpointMap;
     }
     `;
 
@@ -91,28 +130,38 @@ describe('getStatesfromSourceCode', () => {
     expect(states).toStrictEqual([
       {
         slot: 0,
-        type: 'Checkpoint checkpoint; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;}',
-        variable: 'checkpoint'
+        type: 'ActionChoices choice; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;} struct KeyFlag {uint key;bool deleted;} struct ComplexCheckpoint {uint32 fromBlock;mapping (uint => SomeVotes) votes;KeyFlag[] keys;}',
+        variable: 'choice'
       },
       {
         slot: 1,
-        type: 'Checkpoint[] checkpointList; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;}',
-        variable: 'checkpointList'
+        type: 'Checkpoint checkpoint; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;} struct KeyFlag {uint key;bool deleted;} struct ComplexCheckpoint {uint32 fromBlock;mapping (uint => SomeVotes) votes;KeyFlag[] keys;}',
+        variable: 'checkpoint'
       },
       {
         slot: 2,
-        type: 'mapping (address => Checkpoint) checkpointMap; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;}',
-        variable: 'checkpointMap'
+        type: 'Checkpoint[] checkpointList; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;} struct KeyFlag {uint key;bool deleted;} struct ComplexCheckpoint {uint32 fromBlock;mapping (uint => SomeVotes) votes;KeyFlag[] keys;}',
+        variable: 'checkpointList'
       },
       {
         slot: 3,
-        type: 'mapping (address => mapping (uint => Checkpoint)) checkpointNestedMap; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;}',
-        variable: 'checkpointNestedMap'
+        type: 'mapping (address => Checkpoint) checkpointMap; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;} struct KeyFlag {uint key;bool deleted;} struct ComplexCheckpoint {uint32 fromBlock;mapping (uint => SomeVotes) votes;KeyFlag[] keys;}',
+        variable: 'checkpointMap'
       },
       {
         slot: 4,
-        type: 'mapping (address => mapping (uint => NestedCheckpoint)) nestedCheckpointNestedMap; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;}',
+        type: 'mapping (address => mapping (uint => Checkpoint)) checkpointNestedMap; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;} struct KeyFlag {uint key;bool deleted;} struct ComplexCheckpoint {uint32 fromBlock;mapping (uint => SomeVotes) votes;KeyFlag[] keys;}',
+        variable: 'checkpointNestedMap'
+      },
+      {
+        slot: 5,
+        type: 'mapping (address => mapping (uint => NestedCheckpoint)) nestedCheckpointNestedMap; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;} struct KeyFlag {uint key;bool deleted;} struct ComplexCheckpoint {uint32 fromBlock;mapping (uint => SomeVotes) votes;KeyFlag[] keys;}',
         variable: 'nestedCheckpointNestedMap'
+      },
+      {
+        slot: 6,
+        type: 'mapping (address => ComplexCheckpoint) complexCheckpointMap; struct Checkpoint {uint32 fromBlock;uint96 votes;} struct NestedCheckpoint {uint32 fromBlock;SomeVotes votes;} struct SomeVotes {uint96 votes;} struct KeyFlag {uint key;bool deleted;} struct ComplexCheckpoint {uint32 fromBlock;mapping (uint => SomeVotes) votes;KeyFlag[] keys;}',
+        variable: 'complexCheckpointMap'
       }
     ])
   })
